9966aba970203b6e9cdb9a88e003c119f0b1e5a7
Henrik Steen <henrist@henrist.net>
2019-09-03 07:52:08 +0200

    Fix mirror for maven-3
    It seems a new release of Maven is currently in progress, and previous
    releases are removed from the mirror we used, as well as from Apache
    primary source listed at https://maven.apache.org/download.cgi
    
    For now I keep the previous version and wait for an official release
    before any upgrades.

--- tools/maven-3/install.sh
+++ tools/maven-3/install.sh
@@ -7,7 +7,7 @@ set -eux
 
 MAVEN_VERSION=3.6.1
 SHA=b4880fb7a3d81edd190a029440cdf17f308621af68475a4fe976296e71ff4a4b546dd6d8a58aaafba334d309cc11e638c52808a4b0e818fc0fd544226d952544
-BASE_URL=https://apache.osuosl.org/maven/maven-3/${MAVEN_VERSION}/binaries
+BASE_URL=http://apache.uib.no/maven/maven-3/${MAVEN_VERSION}/binaries
 
 mkdir -p /usr/share/maven /usr/share/maven/ref
 wget -O /tmp/apache-maven.tar.gz ${BASE_URL}/apache-maven-${MAVEN_VERSION}-bin.tar.gz
